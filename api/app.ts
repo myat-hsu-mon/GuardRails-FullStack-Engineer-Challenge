967f7f521cd39a69cb154cb829450780e5df3087
myat-hsu-mon <myathsumon554@gmail.com>
2022-08-24 12:22:12 +0630

    add security middlewares

--- api/app.ts
+++ api/app.ts
@@ -2,6 +2,10 @@ import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
+
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
 //app
 import app from "./server";
 //routes
@@ -11,11 +15,20 @@ import errorController from "./controllers/errorController";
 //utils
 import AppError from "./utils/appError";
 
+const limiter = rateLimiter({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many attempts for this ip, please try again in an hour.",
+});
+
 //middlewares
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 app.use(cors());
-app.use(morgan("dev"));
-app.use(helmet());
+app.use(morgan("dev")); // logger
+app.use(helmet()); // To set secure http headers
+app.use("/api", limiter); // To limit too many attempts(Brute-force Attack)
+app.use(xss()); // To prevent xss attack
+app.use(mongoSanitize()); // To prevent nosql injections
 
 app.use("/api/v1/results", resultRoute);
 app.all("*", (req: Request, res: Response, next: NextFunction) => {
