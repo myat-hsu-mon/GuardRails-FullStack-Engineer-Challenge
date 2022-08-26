2eebe6c37429dd304642e2c3020f6f3bfbe9c6aa
myat-hsu-mon <myathsumon554@gmail.com>
2022-08-26 22:57:09 +0630

    update service and package.json for docker compose

--- dashboard/src/config/service.js
+++ dashboard/src/config/service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const service = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: "/api/v1/",
   headers: {
     "Content-Type": "application/json",
   },
