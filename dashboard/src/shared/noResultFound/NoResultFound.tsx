9a9d0abcf754d07c26086f384ecd7bcf36cee75a
myat-hsu-mon <myathsumon554@gmail.com>
2022-08-26 19:13:45 +0630

    add NoResultFound page

---
+++ dashboard/src/shared/noResultFound/NoResultFound.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+import { Grid, Header } from "semantic-ui-react";
+
+const NoResultFound: FC<{ text: string }> = ({ text }) => {
+  return (
+    <Grid
+      textAlign="center"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "100%",
+      }}
+    >
+      <img src="no-result.svg" alt="No Search Result Found!" />
+      <Header as="h5" inverted>
+        {text}
+      </Header>
+    </Grid>
+  );
+};
+
+export default NoResultFound;
